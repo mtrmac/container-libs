26e4421d7fad0c7c4e4e220af4fca5197c7bbd7e
Lokesh Mandvekar <lsm5@redhat.com>
2025-10-24 19:08:49 -0400

    cgroups: remove ErrCgroupV1Rootless code
    Signed-off-by: Lokesh Mandvekar <lsm5@redhat.com>

--- common/pkg/cgroups/cgroups_linux.go
+++ common/pkg/cgroups/cgroups_linux.go
@@ -28,9 +28,7 @@ import (
 var (
 	// ErrCgroupDeleted means the cgroup was deleted.
 	ErrCgroupDeleted = errors.New("cgroup deleted")
-	// ErrCgroupV1Rootless means the cgroup v1 were attempted to be used in rootless environment.
-	ErrCgroupV1Rootless = errors.New("no support for CGroups V1 in rootless environments")
-	ErrStatCgroup       = errors.New("no cgroup available for gathering user statistics")
+	ErrStatCgroup    = errors.New("no cgroup available for gathering user statistics")
 
 	isUnifiedOnce sync.Once
 	isUnified     bool
