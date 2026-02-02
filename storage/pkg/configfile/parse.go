5015728063fbaf0d8b7456051afd38b37e81e1fb
Paul Holzinger <pholzing@redhat.com>
2026-02-02 19:22:50 +0100

    storage/pkg/configfile: add some more error wrapping
    The toml decoder does not mention the path so we need to wrap it in,
    without it users won't know which file has the invalid syntax.
    
    Also add a some context on module open error so users know the issue is
    with the module not the main config files.
    
    Signed-off-by: Paul Holzinger <pholzing@redhat.com>

--- storage/pkg/configfile/parse.go
+++ storage/pkg/configfile/parse.go
@@ -2,6 +2,7 @@ package configfile
 
 import (
 	"errors"
+	"fmt"
 	"io"
 	"io/fs"
 	"iter"
@@ -212,7 +213,7 @@ func Read(conf *File) iter.Seq2[*Item, error] {
 			for _, module := range conf.Modules {
 				f, err := resolveModule(module, dirs)
 				if err != nil {
-					yield(nil, err)
+					yield(nil, fmt.Errorf("could not resolve module: %w", err))
 					return
 				}
 				resolvedModules = append(resolvedModules, f.Name())
@@ -354,7 +355,7 @@ func ParseTOML(configStruct any, conf *File) error {
 		}
 		meta, err := toml.NewDecoder(item.Reader).Decode(configStruct)
 		if err != nil {
-			return err
+			return fmt.Errorf("decode configuration %q: %w", item.Name, err)
 		}
 		keys := meta.Undecoded()
 		if len(keys) > 0 {
