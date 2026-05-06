mtrmac/container-libs

commit 363943ac4b5ef166cdc4c8a2728ded732109eeb1

storage, overlay: use openat2 instead of using procfs
Signed-off-by: Giuseppe Scrivano <gscrivan@redhat.com>

storage/drivers/overlay/overlay.go
@@ -2057,17 +2057,27 @@ func (g *overlayFileGetter) Get(path string) (io.ReadCloser, error) {
 	buf := make([]byte, unix.PathMax)
 	for _, d := range g.diffDirs {
 		if f, found := g.composefsMounts[d]; found {
-			// there is no *at equivalent for getxattr, but it can be emulated by opening the file under /proc/self/fd/$FD/$PATH
-			len, err := unix.Getxattr(fmt.Sprintf("/proc/self/fd/%d/%s", int(f.Fd()), path), "trusted.overlay.redirect", buf)
+			cfd, err := unix.Openat2(int(f.Fd()), path, &unix.OpenHow{
+				Flags:   unix.O_CLOEXEC | unix.O_PATH,
+				Resolve: unix.RESOLVE_NO_SYMLINKS | unix.RESOLVE_BENEATH,
+			})
+			if err != nil {
+				if errors.Is(err, unix.ENOENT) {
+					continue
+				}
+				return nil, &fs.PathError{Op: "openat2", Path: path, Err: err}
+			}
+			n, err := unix.Fgetxattr(cfd, "trusted.overlay.redirect", buf)
+			unix.Close(cfd)
 			if err != nil {
 				if errors.Is(err, unix.ENODATA) {
 					continue
 				}
-				return nil, &fs.PathError{Op: "getxattr", Path: path, Err: err}
+				return nil, &fs.PathError{Op: "fgetxattr", Path: path, Err: err}
 			}
 
 			// the xattr value is the path to the file in the composefs layer diff directory
-			return os.Open(filepath.Join(d, string(buf[:len])))
+			return os.Open(filepath.Join(d, string(buf[:n])))
 		}
 
 		f, err := os.Open(filepath.Join(d, path))

commit 83a097b43b3d8db6852cefa88324635f3e20f413

storage: fix broken fgetxattr call
As documented in the open man page O_PATH is not valid for fgetxattr()
and fails with EBADF.

Fixes: 363943ac4b ("storage, overlay: use openat2 instead of using procfs")

Signed-off-by: Paul Holzinger <pholzing@redhat.com>

storage/drivers/overlay/overlay.go
@@ -2058,7 +2058,7 @@ func (g *overlayFileGetter) Get(path string) (io.ReadCloser, error) {
 	for _, d := range g.diffDirs {
 		if f, found := g.composefsMounts[d]; found {
 			cfd, err := unix.Openat2(int(f.Fd()), path, &unix.OpenHow{
-				Flags:   unix.O_CLOEXEC | unix.O_PATH,
+				Flags:   unix.O_RDONLY | unix.O_CLOEXEC,
 				Resolve: unix.RESOLVE_NO_SYMLINKS | unix.RESOLVE_BENEATH,
 			})
 			if err != nil {