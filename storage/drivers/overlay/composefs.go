cf87d979c13574589316f53fff684fb508fa0372
Giuseppe Scrivano <gscrivan@redhat.com>
2026-06-10 11:24:44 +0000

    composefs: set FSMOUNT_CLOEXEC on fsmount
    Prevent the mount fd from leaking to child processes.
    
    Signed-off-by: Giuseppe Scrivano <gscrivan@redhat.com>

--- storage/drivers/overlay/composefs.go
+++ storage/drivers/overlay/composefs.go
@@ -181,7 +181,7 @@ func openBlobFile(blobFile string, hasACL, useLoopDevice bool) (int, error) {
 		return -1, fmt.Errorf("failed to create erofs filesystem: %w", err)
 	}
 
-	mfd, err := unix.Fsmount(fsfd, 0, unix.MOUNT_ATTR_RDONLY)
+	mfd, err := unix.Fsmount(fsfd, unix.FSMOUNT_CLOEXEC, unix.MOUNT_ATTR_RDONLY)
 	if err != nil {
 		buffer := make([]byte, 4096)
 		if n, _ := unix.Read(fsfd, buffer); n > 0 {
