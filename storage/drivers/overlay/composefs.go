6dfd7fd8a365cbb405b547bf2dea621fdb145882
Giuseppe Scrivano <gscrivan@redhat.com>
2026-06-10 11:24:38 +0000

    composefs: set FSOPEN_CLOEXEC on fsopen
    Prevent the filesystem context fd from leaking to child processes.
    
    Signed-off-by: Giuseppe Scrivano <gscrivan@redhat.com>

--- storage/drivers/overlay/composefs.go
+++ storage/drivers/overlay/composefs.go
@@ -153,7 +153,7 @@ func openBlobFile(blobFile string, hasACL, useLoopDevice bool) (int, error) {
 		blobFile = loop.Name()
 	}
 
-	fsfd, err := unix.Fsopen("erofs", 0)
+	fsfd, err := unix.Fsopen("erofs", unix.FSOPEN_CLOEXEC)
 	if err != nil {
 		return -1, fmt.Errorf("failed to open erofs filesystem: %w", err)
 	}
