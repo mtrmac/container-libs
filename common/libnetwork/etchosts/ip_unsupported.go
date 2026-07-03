9931354de01aa0b372c6fee1121a046a10cd1f2a
Miloslav Trmač <mitr@redhat.com>
2026-07-03 20:21:28 +0200

    Fix lint on non-Linux
    > libnetwork/etchosts/ip_unsupported.go:6:1: Comment should end in a period (godot)
    > // (should never happen)
    
    Signed-off-by: Miloslav Trmač <mitr@redhat.com>

--- common/libnetwork/etchosts/ip_unsupported.go
+++ common/libnetwork/etchosts/ip_unsupported.go
@@ -3,7 +3,7 @@
 package etchosts
 
 // wslHostIP returns an empty string when running on OSes other than Linux
-// (should never happen)
+// (should never happen).
 func wslHostIP() string {
 	return ""
 }
